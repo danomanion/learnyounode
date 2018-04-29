9afb3a4f337ad24ac8a8fbb062a585226ea34143
Dano Manion <danomanion@gmail.com>
2018-04-29 07:28:00 -0600

    add IO asyc example

--- io_async.js
+++ io_async.js
@@ -1,10 +1,10 @@
 let fs = require('fs');
 let filePath = process.argv[2];
 
-fs.readFile(filePath, function(err, data) {
+fs.readFile(filePath, function (err, data) {
   if (err) {
     throw err;
-  };
+  }
   let lines = data.toString().split('\n').length - 1;
   console.log(lines);
 });
